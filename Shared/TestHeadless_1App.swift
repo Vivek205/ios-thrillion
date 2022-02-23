12dc45ccb541613aba33f91a811400e60ebbaa6e
Vivek205 <vnarayanan@travix.com>
2022-02-23 15:07:19 +0100

    Initial Commit

---
+++ Shared/TestHeadless_1App.swift
@@ -0,0 +1,17 @@
+//
+//  TestHeadless_1App.swift
+//  Shared
+//
+//  Created by Vivek Narayanan on 23/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct TestHeadless_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
